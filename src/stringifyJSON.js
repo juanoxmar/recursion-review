b755f2b16c0847f32b86df3bd1a01fab2de200cb
Juan Oxmar <joxmar.ramirez@gmail.com>
2020-11-16 15:28:08 -0800

    Completed stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,6 +3,38 @@
 
 // but you don't so you're going to write it from scratch:
 
-var stringifyJSON = function(obj) {
+var stringifyJSON = function (obj) {
   // your code goes here
+  if (typeof obj === 'string') {
+    return `"${obj}"`;
+  }
+
+  const typeCheck =
+    typeof obj === 'number' ||
+    typeof obj === 'boolean' ||
+    typeof obj === 'undefined' ||
+    obj === null;
+
+  if (typeCheck) {
+    return `${obj}`;
+  }
+
+  if (Array.isArray(obj)) {
+    const arr = _.map(obj, (item) => stringifyJSON(item));
+
+    return `[${arr}]`;
+  }
+
+  // object strings
+  const objStrings = [];
+
+  // looping through object to push keys & values to objStrings
+  for (let k in obj) {
+    if (typeof obj[k] !== 'function' && typeof obj[k] !== 'undefined') {
+      objStrings.push(`"${k}":${stringifyJSON(obj[k])}`);
+    }
+  }
+
+  // be able to put curly brackets around the objStrings before returned
+  return `{${objStrings}}`;
 };
